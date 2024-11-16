33c829dea62ecc2523869272369fe53cab74f6e6
lucbea <profluciaferrer@gmail.com>
2024-11-16 16:05:05 -0300

    pagina 404

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -3,10 +3,12 @@ import { Box } from '@mui/material';
 import Emoji404 from '../assets/emoji404.png';
 
 export const Error404 = () => {
+    console.log("404");
     return (
         <Box sx={{ paddingTop: '20px', margin: 'auto', textAlign: 'center' }}>
             <h2>Uuups!</h2>
             <h2>404 - NOT FOUND</h2>
+            
             <Box sx={{height:'200px', }}>
                 <img src={Emoji404} alt="404 Emoji" style={{ maxWidth: '100%', height: 'inherit' }} />
             </Box>
